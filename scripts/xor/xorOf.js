3851bb9c4413c430858cddb62f0baf8bd2fb2b63
flip <ansuz@transitiontech.ca>
2014-12-09 10:13:19 -0500

    comments about usage

--- scripts/xor/xorOf.js
+++ scripts/xor/xorOf.js
@@ -1,5 +1,22 @@
 var xor=require("./xorfc.js");
 
+/*
+  Call this script in one of three ways:
+
+  // xor your ipv6 (if active) against the passed argument
+  node xorOf <ipv6>
+
+  // xor the two passed arguments
+  node xorOf <ipv61> <ipv62>
+
+  // pass 3 args, a,b,c
+  // return 0 if b and c are equidistant from a
+  // return -1 if b is closer, else return 1
+  node xorOf <ipv61> <ipv62> <ipv63>
+
+*/
+
+
 var args=process.argv;
 
 switch(args.length){
