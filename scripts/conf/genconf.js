30583f13e62cf37f291e7e78024b846545739f18
ansuz <ansuz@transitiontech.ca>
2014-12-20 13:35:58 -0500

    generate full credentials

--- scripts/conf/genconf.js
+++ scripts/conf/genconf.js
@@ -1,5 +1,6 @@
 var std=require("./default.json");
 var passwd=require("../keys/passwd.js");
+var keys=require("../keys/keys.js");
 
 // set admin password 
 std.admin.password=passwd.gen(31);
@@ -16,6 +17,10 @@ std.authorizedPasswords=[31.31,31,31,31]
 std.interfaces.UDPInterface[0].bind="0.0.0.0:"+
   (Math.floor(Math.random()*55535)+10000);
 
-
+// how about some creds?
+var creds=keys.genAddress();
+std.privateKey=creds.hexPriv;
+std.publicKey=creds.b32pub;
+std.ipv6=creds.ipv6;
 
 console.log(JSON.stringify(std));
